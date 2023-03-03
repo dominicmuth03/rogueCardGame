3c9dae9bd46662304026ca29474418778cd36f64
dominicmuth03 <dominic.muth03@gmail.com>
2023-03-03 14:44:04 -0500

    commit gen

--- untitled folder 3/Cards/CardClasses.java
+++ untitled folder 3/Cards/CardClasses.java
@@ -1,14 +1,73 @@
 package Cards;
-
+include package Characters;
+include package Enemies;
 
 abstract class Card {
-    int AP;
+    String name;
+    String description;
+    String attribute; // maybe convert to int later at create method to return as string later to save proccesing power?
+    int AP; //Action Points
     boolean DOR; //draw or exile; true if draw, false if remove
-    abstract void applyCard();
-    abstract boolean canPlay();
+    abstract void cardEffects();
+    void applyCard() {
+        if (canPlay()) {
+            cardEffects();
+        } else {
+            printCantPlay();
+        }
+    };
+    void canPlay() {
+        if (Player.player.AP >= this.AP) {
+            Player.player.AP -= this.AP;
+            return true;
+        }
+        return false;
+    };
+    int requestTarget();
+    void printCantPlay();
 }
 
-abstract class SingleTargetCards extends Card {
-    abstract int requestTarget();
+public class Underhaw extends Card {
+    Underhaw() {
+        name = "Underhaw";
+        description = "Deals 2d4 + 2 damage to a single Enemy";
+        attribute = "Blade";
+        AP = 1;
+        DOR = true;
+    };
+    void cardEffects() {
+        int target = requestTarget();
+        int damage = 2 + Player.player.d4.roll() + Player.player.d4.roll();
+        Enemies.enemies.get(target).takeDamage(damage);
+    };
 }
 
+public class Mittlehaw extends Card {
+    Underhaw() {
+        name = "Mittlehaw";
+        description = "Deals 1d6 damage to all Enemies";
+        attribute = "Blade";
+        AP = 1;
+        DOR = true;
+    };
+    void cardEffects() {
+        int target = requestTarget();
+        int damage = 2 + Player.player.d4.roll() + Player.player.d4.roll();
+        Enemies.enemies.get(target).takeDamage(damage);
+    };
+}
+
+public class Mittlehaw extends Card {
+    Underhaw() {
+        name = "Mittlehaw";
+        description = "Deals 1d6 damage to all Enemies";
+        attribute = "Blade";
+        AP = 1;
+        DOR = true;
+    };
+    void cardEffects() {
+        int target = requestTarget();
+        int damage = 2 + Player.player.d4.roll() + Player.player.d4.roll();
+        Enemies.enemies.get(target).takeDamage(damage);
+    };
+}
--- untitled folder 3/Die/Dice.java
+++ untitled folder 3/Die/Dice.java
@@ -3,4 +3,29 @@ import java.lang.Math;
 
 abstract class Dice {
     abstract public void preroll();
+    abstract public int roll();
+}
+
+public class D4 extends Dice {
+
+}
+
+public class D6 extends Dice {
+    
+}
+
+public class D8 extends Dice {
+    
+}
+
+public class D10 extends Dice {
+    
+}
+
+public class D12 extends Dice {
+    
+}
+
+public class D20 extends Dice {
+    
 }
