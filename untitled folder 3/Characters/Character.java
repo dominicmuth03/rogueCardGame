d14e93d9f668973d5df12175fd9751c1d05f3de2
dominicmuth03 <dominic.muth03@gmail.com>
2023-03-03 14:44:31 -0500

    commit gen

---
+++ untitled folder 3/Characters/Character.java
@@ -0,0 +1,15 @@
+package Characters;
+public class Player {
+    public static Character player;
+}
+
+abstract public class Character {
+    int AP;
+    int armour;
+    D4 d4;
+    D6 d6;
+    D8 d8;
+    D10 d10;
+    D12 d12;
+    D20 d20;
+}
---
+++ untitled folder 3/Enemies/Monster.java
@@ -0,0 +1,9 @@
+package Enemies;
+
+public class Enemies {
+    public static Monster enemies[];
+}
+
+abstract public class Monster {
+    void takeDamage(int damage);
+}
