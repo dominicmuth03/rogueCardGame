47fea683f3aa11a2eda5007c4158e1f6c087c2de
dominicmuth03 <104279394+dominicmuth03@users.noreply.github.com>
2023-03-08 07:51:56 -0500

    Add files via upload

--- untitled folder 3/Die/Dice.java
+++ untitled folder 3/Die/Dice.java
@@ -1,14 +1,6 @@
 package Die;
-// import java.lang.Math;
+import java.lang.Math;
 
 abstract class Dice {
     abstract public void preroll();
-    abstract public int roll();
-    public int rollnTimes(int n) {
-        int retval = 0;
-        for (int i = 0; i < n; i++) {
-            retval += roll();
-        }
-        return retval;
-    }
-};
+}
